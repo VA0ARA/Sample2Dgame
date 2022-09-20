2418f502be3926eedbc236308656127d49ed283a
vahid ara <vahidaara@gmail.com>
2022-09-20 20:09:15 +0430

    add Input in function

--- Player.cs
+++ Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    public float MoveForce = 10f;
+    [SerializeField]
+    public float jumpForce = 11f;
+    private float MovmentX;
+    private Rigidbody2D mybody;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private string WAlkANImation = "walk";
+    private void Awake()
+    {
+        // just intialaze compotion 
+        mybody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlyerMOveKeyboard();
+
+
+    }
+    private void PlyerMOveKeyboard()
+    {
+        MovmentX = Input.GetAxisRaw("Horizontal");
+        // Debug.Log(MovmentX);
+        transform.position += new Vector3(MovmentX, 0f, 0f)*Time.deltaTime*MoveForce; 
+
     }
 }
